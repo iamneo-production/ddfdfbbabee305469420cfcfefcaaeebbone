7efb516968751b19b609d72ceeab954198e457d2
Iamneo <techadmin@iamneo.ai>
2023-09-27 08:26:44 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,32 +1,30 @@
-import React, { useState } from 'react';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
-  const [name, setName] = useState('');
-  const [greeting, setGreeting] = useState('');
 
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
+  const [name,setName] = useState('');
+  const[greeting,setGreeting] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setGreeting(`Hello, ${name}!`);
-  };
+const handleChange = (e) =>{
+  setName(e.target.value);
+}
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setGreeting(`Hello,${name}`);
+}
   return (
     <div className="App">
-      <h2>Hey!!! Greeting</h2>
-      <form onSubmit={handleSubmit} data-testid="form">
-        <label data-testid="label">Enter your name: </label>
-        <input
-          type="text"
-          value={name}
-          onChange={handleChange}
-        />
-        <button type="submit" data-testid="buttonElement">Say Hello</button>
-      </form>
-      {greeting && <h2>{greeting}</h2>}
+      <div className="contents">
+        <h3>Hey!!! Greeting</h3>
+        <form onSubmit={handleSubmit} data-testid="form">
+          <label data-testid="label">Enter name :</label>
+          <input type="text" value={name} onChange={handleChange}/>
+          <button type="submit" data-testid="buttonElement">Say Hello</button>
+          </form>
+          {greeting && <h2>{greeting}</h2>}
+    </div>
     </div>
   );
 }
